25d34b3c2c4cc000b6bea27db9bbd89e9ba94e19
Matan Greenvlad <mgk2798@gmail.com>
2020-12-17 14:43:34 +0200

    canvas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { H1, StyledDiv, StyledUl, Wrapper } from './styles/styledComponents';
 import { Button } from "@material-ui/core";
-const image1 = require('./pics/gift-box-1.png')
+import image1 from './pics/gift-box-1.jpg'
+import image2 from './pics/gift-box-2.jpg'
+import image3 from './pics/gift-box-3.jpg'
 
 function List() {
 
@@ -43,18 +45,44 @@ const Canvas = ({width, height} : CanvasProps) => {
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const contextRef = useRef(null);
-    const giftBox1: any = new Image()
-    giftBox1.src = image1;
+  
 
   useEffect(() => {
     const canvas : HTMLCanvasElement | null = canvasRef.current;
     canvas!.width = window.innerWidth;
     canvas!.height = window.innerHeight;
     const ctx = canvas?.getContext("2d");
+    const giftBox1: any = new Image(40, 40)
+    const giftBox2: any = new Image(40, 40) 
+    const giftBox3: any = new Image(35,35) 
+    giftBox1.src = image1;
+    giftBox2.src = image2;
+    giftBox3.src = image3;
+    const images: HTMLImageElement[] = [giftBox1, giftBox2, giftBox3]
+    giftBox3.onload = () => {
+      for (let i = 0; i < 12; i++) {
+        const latency: number = Math.random() * 2000;
+          setTimeout(() => {
+            let yAxis = 0
+            let xAxis = (i * 120)
+            giftsColumn(xAxis, yAxis, 5, 3)
+          }, latency);
+      }
+      
+    }
+
+    function giftsColumn(xAxis: number, yAxis: number, reapet: number, ppi: number) {
+      const currImage: HTMLImageElement = images[Math.floor(Math.random() * 3)]
+      setInterval(() => {
+        ctx!.clearRect(xAxis, yAxis - ppi, 100, 100)
+        ctx!.drawImage(currImage, xAxis, yAxis, 100, 100);
+        if (yAxis === Math.floor(canvas!.height / ppi / reapet) * ppi) {
+          giftsColumn(xAxis, 0, reapet, ppi)
+        }
+        yAxis += ppi
+      }, [20]);  
+    };
     
-    ctx!.drawImage(giftBox1, 50, 80)
-    console.log(giftBox1);
-    ctx!.stroke();
     // ctx!.fillStyle = 'red';
     // ctx!.fillRect(20, 20, 50, 50)
     // console.log(ctx, canvas)
@@ -65,7 +93,7 @@ const Canvas = ({width, height} : CanvasProps) => {
   
 
   return (
-    <canvas ref={canvasRef} >{giftBox1 }</canvas> 
+    <canvas ref={canvasRef} ></canvas> 
   )
 }
 
@@ -81,6 +109,7 @@ function App  () {
             <Link to="/received">Received</Link>
           </li>
         </ul>
+        
       </div>
       <Route exact path="/list" component={List}></Route>
       <Route exact path="/received" component={Received}></Route>
