c2e342e1a41d58576c21aabee99bfe0b71fc1385
Matan Greenvlad <mgk2798@gmail.com>
2020-12-18 03:06:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path") ;
 
 const app = express();
-app.use(express.static("./client/build"));
+app.use(express.static("build"));
 app.use(express.json());
 
 app.use("/api", require("./api"));
-app.get("*", (req , res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// app.get("*", (req , res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
 module.exports =  app;
