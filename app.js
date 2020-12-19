8f34f14dd706039eae9bf960b33e4fc05ccaeda6
Matan Greenvlad <mgk2798@gmail.com>
2020-12-19 18:56:54 +0200

    build relocation

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path") ;
 
 const app = express();
-app.use(express.static("client/build"));
+app.use(express.static("build"));
 app.use(express.json());
 
 const dir_name = process.cwd()
 
 app.use("/api", require("./api"));
 app.get("*", (req , res) => {
-  res.sendFile(path.resolve(dir_name, "client/build", "index.html"));
+  res.sendFile(path.resolve(dir_name, "build", "index.html"));
 });
 
 module.exports =  app;
