e5af39fc72df7890f621dcf1a8b4f67b17799c9f
Matan Greenvlad <mgk2798@gmail.com>
2020-12-19 18:54:33 +0200

    build client

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path") ;
 
 const app = express();
-app.use(express.static("build"));
+app.use(express.static("client/build"));
 app.use(express.json());
 
 const dir_name = process.cwd()
